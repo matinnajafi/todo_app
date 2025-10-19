1070e079f2d74071ba82a97063961bfa86b40fdc
Matin <matin7najafi@gmail.com>
2025-10-19 14:25:22 +0330

    Implement done task list structure

--- lib/presentation/screens/task_screen.dart
+++ lib/presentation/screens/task_screen.dart
@@ -6,8 +6,15 @@ import 'package:todo_app/presentation/widgets/search_box.dart';
 import 'package:todo_app/presentation/widgets/task_widget.dart';
 import 'package:todo_app/presentation/widgets/user_info.dart';
 
-class TaskScreen extends StatelessWidget {
+class TaskScreen extends StatefulWidget {
   const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  bool isDoneTasksExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +32,94 @@ class TaskScreen extends StatelessWidget {
             const SliverToBoxAdapter(child: CategoryList()),
             const SliverPadding(padding: EdgeInsets.only(bottom: 4)),
             SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return const TaskWidget();
-              }, childCount: 4),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => const TaskWidget(),
+                childCount: 4,
+              ),
             ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 24, left: 24, bottom: 12),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isDoneTasksExpanded = !isDoneTasksExpanded;
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AnimatedRotation(
+                            turns:
+                                isDoneTasksExpanded
+                                    ? -0.5
+                                    : 0.0, // 180° or default
+                            duration: const Duration(milliseconds: 250),
+                            child: SizedBox(
+                              height: 20,
+                              width: 20,
+                              child: Image.asset('images/black_arrow.png'),
+                            ),
+                          ),
+                          const Text(
+                            'تسک‌های انجام شده',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 21,
+                            width: 21,
+                            child:
+                                isDoneTasksExpanded
+                                    ? IconButton(
+                                      padding: EdgeInsets.zero,
+                                      iconSize: 21,
+                                      onPressed: () {
+                                        setState(() {
+                                          isDoneTasksExpanded = false;
+                                          // clear done task list
+                                        });
+                                      },
+                                      icon: Image.asset(
+                                        'images/black_clear.png',
+                                      ),
+                                    )
+                                    : const SizedBox(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            if (isDoneTasksExpanded)
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => Column(
+                    children: const [
+                      Opacity(
+                        opacity: 0.5,
+                        child: IgnorePointer(child: TaskWidget()),
+                      ),
+                      SizedBox(height: 1),
+                    ],
+                  ),
+                  childCount: 2,
+                ),
+              ),
+            const SliverPadding(padding: EdgeInsets.only(bottom: 24)),
           ],
         ),
       ),
